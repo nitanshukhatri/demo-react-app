9d5773818daead15c0ddf4a12809d2821f1b182f
Nitanshu <nitanshukhatri@yahoo.in>
2018-12-09 23:57:24 +0530

    logger

--- src/index.js
+++ src/index.js
@@ -12,6 +12,8 @@ import { store } from "./_helpers/store";
 import { configureFakeBackend } from "./_helpers/fake-backend";
 import logger from "./_services/log.service";
 
+logger.init();
+
 configureFakeBackend();
 
 ReactDOM.render(
